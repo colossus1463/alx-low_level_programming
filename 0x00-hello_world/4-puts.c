7117247f3ff6c161842d30d04730fbe4362c5d1a
colossus 1463 <olukayodeniyiezekiel@gmail.com>
2022-09-08 19:32:07 -0700

    my first commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry region
+ * Return: Always 0 (Good)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"),
+		return (0);
+}
