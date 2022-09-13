ff1d845671a39480fb5266915a7bda1bdc4019a2
colossus 1463 <olukayodeniyiezekiel@gmail.com>
2022-09-13 04:57:20 +0000

    t

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: 0
  */
 int main(void)
 {
@@ -17,13 +17,13 @@ int main(void)
 			{
 				putchar((i / 10) + '0');
 				putchar((i % 10) + '0');
-				putchar('');
+				putchar(' ');
 				putchar((p / 10) + '0');
 				putchar((p % 10) + '0');
 				if (i != 98)
 				{
 					putchar(',');
-					putchar('');
+					putchar(' ');
 				}
 			}
 		}
