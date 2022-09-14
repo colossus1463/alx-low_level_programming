bf2d8efedbb7cfc60e52cabf9aef546e3d46b9a7
colossus 1463 <olukayodeniyiezekiel@gmail.com>
2022-09-14 04:53:24 +0000

    h

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ */
+void times_table(void)
+{
+	int num, nult, prod;
+	for (num = 0; num <= 9; num ++)
+	{
+		_putchar('0');
+		for (nult = 1; nult <= 9; nult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			prod = num * nult;
+			if (prod <= 9)
+
+				_putchar((prod / 10) + '0');
+			_putchar((prod % 10)+ '0');
+		}
+		_putchar('\n');
+	}
+}
+
