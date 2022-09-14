4b20e8ddaa50e05b0111bf2b9fed4af1aff901ed
colossus 1463 <olukayodeniyiezekiel@gmail.com>
2022-09-14 02:29:12 +0000

    k

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function that prints alphabet 10 times
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	char c, i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+		_putchar('\n');
+	}}
