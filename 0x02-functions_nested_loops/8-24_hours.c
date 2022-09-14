ba0d45158c13c78d9c579816dfa5ac37fe19db41
colossus 1463 <olukayodeniyiezekiel@gmail.com>
2022-09-14 04:35:53 +0000

    h

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * jack_bauer - function that prints every minutes of the day,start
+ * n from 00:00 t0 23:59
+ * Return: always 0
+ */
+void jack_bauer(void)
+{
+	int hours = 0;
+	int minutes = 0;
+	int hours_remainder;
+	int mins_remainder;
+
+	while (hours <= 23)
+	{
+		while (minutes <= 59)
+		{
+			mins_remainder = minutes % 10;
+			hours_remainder = hours % 10;
+			_putchar(hours / 10 + '0');
+			_putchar(hours_remainder + '0');
+			_putchar(':');
+			_putchar(minutes / 10 + '0');
+			_putchar 9mins_remainder + '0');
+			minutes++;
+			_putchar('\n');
+		}
+		hours++;
+		minutes = 0;
+	}}
