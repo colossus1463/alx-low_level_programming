96589e3b73b4a247fb528ae8834fc1da9225c384
colossus 1463 <olukayodeniyiezekiel@gmail.com>
2022-09-15 02:48:10 +0000

    g

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -13,12 +13,11 @@
 int main(void)
 {
 		int sum, num;
-
-			for (num = 0; num < 1024; ++num)
-					{
-						if ((num % 3 == 0) || (num % 5 == 0))
-							sum += num;
-					}
-			printf("%d\n", sum);
-			return (0);
+		for (num = 0; num < 1024; ++num)
+		{
+			if ((num % 3 == 0) || (num % 5 == 0))
+				sum += num;
+		}
+		printf("%d\n", sum);
+		return (0);
 }
