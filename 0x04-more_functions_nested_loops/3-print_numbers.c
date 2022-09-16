4b5f949532f571e5e44f12f75773822da42eecae
colossus1463 <olukayodeniyiezekiel@gmail.com>
2022-09-16 04:40:43 +0100

    g

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,18 +2,18 @@
 #include "main.h"
 
 /**
- * print_numbers - function that print all numbers from
- * o to 9
- * Return: returns nothing
+ *print_numbers - function that print all numbers from
+ *o to 9
+ *Return: returns nothing
  */
 
 void print_numbers(void)
 {
+
 	int n;
-	
+
 	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
 	}
 	putchar(10);
-}
